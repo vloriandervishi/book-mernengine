9cffef1fb18c742b58a0612d8b0ec7665698fb53
Valdo <vlorian22@GMAIL.COM>
2021-03-16 21:03:30 -0500

    created mutation js file in utils folder

---
+++ client/src/utils/mutation.js
@@ -0,0 +1,81 @@
+import gql from "graphql-tag";
+import { saveBook } from "./API";
+
+export const LOGIN_USER= gql`
+ mutation login($email: String!, $password: String!){
+     login(email:$email,password: $password){
+         token
+         user {
+             _id
+             username
+             email
+             bookCount
+             savedBooks {
+                 bookId
+                 title
+                 description
+                 authors
+                 image
+                 link
+             }
+         }
+     }
+ }
+`;
+export const ADD_USER = gql `
+mutation addUser($username: String!, $email: String!, $password: String!){
+    addUser(username: $username, email:$email, password: $password){
+        token
+        user{
+            _id
+            username
+            email
+            bookCount
+            savedBooks{
+                bookId
+                title
+                description
+                authors
+                image
+                link
+            }
+        }
+    }
+}
+`;
+export const SAVE_BOOK = gql `
+ mutation saveBook ($input: bookInfeed){
+     saveBook(input: $input){
+         _id
+         username
+         email
+         savedBooks{
+             bookId
+             authors
+             image
+             description
+             title
+             link
+         }
+     }
+ }
+ `;
+
+ export const REMOVE_Book = gql`
+ mutation removeBook($bookId: String!){
+     removeBook(bookId: $bookId){
+         _id
+         username
+         bookCount
+         savedBooks{
+             bookId
+             authors
+             image
+             description
+             title 
+             link
+             
+         }
+     }
+ }
+ `;
